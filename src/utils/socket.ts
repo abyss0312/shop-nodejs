2e2f802e30fea7c6191d7eb1c2a537c4ce4305ad
Alejandro Arias <alejandroarias0312@gmail.com>
2023-01-24 18:47:11 -0400

    list of users

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -5,28 +5,42 @@ export default function  socketNode(io){
     io.on('connection', (socket) =>{
       
         console.log('ws connection');
+        const users = [];
+        for (let [id, socket] of io.of("/").sockets) {
+          users.push({
+            userID: id,
+            username: socket.username,
+          });
+        }
+        console.log(users);
+        socket.emit("users", users);
+
+        socket.broadcast.emit("user connected", {
+          userID: socket.id,
+          username: socket.username,
+        });
 
         socket.on("private_message", ({ content, to }) => {
-            console.log(content);
-            socket.to(to).emit("private message", {
+            socket.to(to).emit("private_message", {
               content,
               from: socket.id,
             });
           });
-    
+       
+       
     })
 
-    io.use(async (socket, next) => {
+     io.use(async (socket, next) => {
         const username = socket.handshake.auth.username;
-        const user = await verifyToken(socket.handshake.auth.id)
+    /*     const user = await verifyToken(socket.handshake.auth.id)
         if (!user) {
           return next(new Error("invalid username"));
-        }
+        } */
         socket.username = username;
-        socket.userId = user.Id;
+        //socket.userId = user.Id;
         console.log(socket.id);
         next();
-      });
+      }); 
 
  
 }
